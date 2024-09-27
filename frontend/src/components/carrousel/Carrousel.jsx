af2ff2c566c28fb1a416d7c11ef5b4689a6cf4fa
unknown <quenea.gislain@gmail.com>
2024-09-27 09:31:29 +0200

    Updated carrousel component, deleted desktop.ini and deleted useless files

--- frontend/src/components/carrousel/Carrousel.jsx
+++ frontend/src/components/carrousel/Carrousel.jsx
@@ -23,17 +23,23 @@ export default function Carrousel({ images }) {
       <div className="carrousel-slide">
         <img src={images[currentIndex]} alt={`Slide ${currentIndex}`} />
       </div>
-      <div className="carrousel-buttons">
-        <button onClick={prevSlide} className="carrousel-button prev">
-          <img src={arrowLeft} alt="Flèche de gauche" />
-        </button>
-        <button onClick={nextSlide} className="carrousel-button next">
-          <img src={arrowRight} alt="Flèche de droite" />
-        </button>
-      </div>
-      <div className="carousel-counter">
-        {currentIndex + 1} / {images.length}
-      </div>
+
+      {images.length > 1 && (
+        <>
+          <div className="carrousel-buttons">
+            <button onClick={prevSlide} className="carrousel-button prev">
+              <img src={arrowLeft} alt="Flèche de gauche" />
+            </button>
+            <button onClick={nextSlide} className="carrousel-button next">
+              <img src={arrowRight} alt="Flèche de droite" />
+            </button>
+          </div>
+
+          <div className="carousel-counter">
+            {currentIndex + 1} / {images.length}
+          </div>
+        </>
+      )}
     </div>
   );
 }
